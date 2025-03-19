468517b965d1305b30a92b8969b3436e6396e705
Lexi Haws <lsh66@drexel.edu>
2025-03-19 14:06:42 -0400

    Update cleaning_sex_rates.R

--- cleaning_sex_rates.R
+++ cleaning_sex_rates.R
@@ -51,7 +51,8 @@ sex_specific_rates <- cases_by_sex_county %>%
   ) %>%
   select(GEOID, County, State, Year, Sex, sex_specific_rate_acsc_c, sex_specific_rate_acute_c, 
          sex_specific_rate_chronic_c, sex_specific_rate_diabetes_c, sex_population_county)
-# Step 6: Calculate crude rates at the county level (using total_sex_population)
+
+# Step 3: Calculate crude rates at the county level (using total_sex_population)
 sex_rates_crude <- cases_by_sex_county %>%
   group_by(GEOID, County, State, Year) %>%
   summarise(
@@ -62,7 +63,7 @@ sex_rates_crude <- cases_by_sex_county %>%
   ) %>%
   ungroup()
 
-# Step 7: Combine sex-specific rates, crude rates, and adjusted rates
+# Step 4: Combine sex-specific rates, crude rates, and adjusted rates
 final_rates_county_sex <- sex_rates_crude %>%
   left_join(sex_specific_rates, by = c("GEOID", "County", "State", "Year"))
 final_rates_county_sex=final_rates_county_sex%>%
@@ -91,7 +92,8 @@ sex_specific_rates_state <- state_data_sex %>%
   select(State, Year, Sex, Sex_specific_rate_acsc_s, Sex_specific_rate_acute_s, 
          Sex_specific_rate_chronic_s, Sex_specific_rate_diabetes_s, 
          sex_population_state)
-# Step 6: Calculate crude rates at the state level
+
+# Step 3: Calculate crude rates at the state level
 sex_rates_crude_state <- state_data_sex %>%
   group_by(State, Year) %>%
   summarise(
@@ -102,7 +104,7 @@ sex_rates_crude_state <- state_data_sex %>%
   ) %>%
   ungroup()
 
-# Step 7: Merge the final results into one data frame
+# Step 4: Merge the final results into one data frame
 final_rates_state_sex <- sex_rates_crude_state %>%
   left_join(sex_specific_rates_state, by = c("State", "Year"))
 final_rates_state_sex <- final_rates_state_sex %>%
@@ -119,6 +121,7 @@ year_data_sex<- merged_data_yearfin%>%
     Acute_Hosp = sum(`Acute Hospitalization`, na.rm = TRUE),
     Chronic_Hosp = sum(`Chronic Hospitalization`, na.rm = TRUE),
     Diabetes_Hosp = sum(`Diabetes Hospitalization`, na.rm = TRUE))%>% ungroup
+
 # Step 2: Calculate sex-specific rates for each year
 sex_specific_rates_year<-year_data_sex %>%
   mutate( 
@@ -131,7 +134,7 @@ sex_specific_rates_year<-year_data_sex %>%
          Sex_specific_rate_chronic_y, Sex_specific_rate_diabetes_y, 
          sex_population_year)
 
-# Step 6: Calculate crude rates at the year level
+# Step 3: Calculate crude rates at the year level
 sex_rates_crude_year <- year_data_sex %>%
   group_by(Year) %>%
   summarise(
@@ -142,7 +145,7 @@ sex_rates_crude_year <- year_data_sex %>%
   ) %>%
   ungroup()
 
-# Step 7: Merge the final results into one data frame
+# Step 4: Merge the final results into one data frame
 final_rates_year_sex <- sex_rates_crude_year %>%
   left_join(sex_specific_rates_year, by = c("Year"))
 final_rates_year_sex <- final_rates_year_sex %>%
@@ -155,25 +158,12 @@ colnames(final_rates_county_sex)
 colnames(final_rates_state_sex)
 colnames(final_rates_year_sex)
 
-duplicates_county_sex <- final_rates_county_sex %>%
-  group_by(State, Year, Sex) %>%
-  filter(n() > 1)
-
-
-duplicates_state_sex <- final_rates_state_sex %>%
-  group_by(State, Year, Sex) %>%
-  filter(n() > 1)
-
 final_rates_county_sex_distinct <- final_rates_county_sex %>%
   distinct(County, State, Year, Sex, .keep_all = TRUE)
 final_rates_state_sex_distinct <- final_rates_state_sex %>%
   distinct(State, Year, Sex, .keep_all = TRUE)
 final_data_sex_rates <- final_rates_county_sex_distinct %>%
   left_join(final_rates_state_sex_distinct, by = c("State", "Year", "Sex"))
-
-duplicates_year_sex <- final_rates_year_sex %>%
-  group_by( Year, Sex) %>%
-  filter(n() > 1)
 
 final_rates_year_sex_distinct <- final_rates_year_sex %>%
   distinct(Year, Sex, .keep_all = TRUE)
@@ -182,7 +172,7 @@ cleaned_data_sex_rates_fin<-final_data_sex_rates%>%
   left_join(final_rates_year_sex_distinct, by=c("Year","Sex"))
 
 save(cleaned_data_sex_rates_fin, file="cleaned_data_sex_final.RData")
-load("cleaned_data_sex_final.RData")
+
 #WORKING POPULATION
 cleaned_merged_data_sexw<-cleaned_merged_dataw7%>%
   select(
@@ -218,7 +208,8 @@ sex_specific_ratesw <- cases_by_sex_countyw %>%
   ) %>%
   select(GEOID, County, State, Year, Sex, sex_specific_rate_acsc_c, sex_specific_rate_acute_c, 
          sex_specific_rate_chronic_c, sex_specific_rate_diabetes_c, sex_population_county)
-# Step 6: Calculate crude rates at the county level (using total_sex_population)
+
+# Step 3: Calculate crude rates at the county level (using total_sex_population)
 sex_rates_crudew <- cases_by_sex_countyw %>%
   group_by(GEOID, County, State, Year) %>%
   summarise(
@@ -229,7 +220,7 @@ sex_rates_crudew <- cases_by_sex_countyw %>%
   ) %>%
   ungroup()
 
-# Step 7: Combine sex-specific rates, crude rates, and adjusted rates
+# Step 4: Combine sex-specific rates, crude rates, and adjusted rates
 final_rates_county_sexw <- sex_rates_crudew %>%
   left_join(sex_specific_ratesw, by = c("GEOID", "County", "State", "Year"))
 final_rates_county_sexw=final_rates_county_sexw%>%
@@ -258,7 +249,8 @@ sex_specific_rates_statew <- state_data_sexw %>%
   select(State, Year, Sex, Sex_specific_rate_acsc_s, Sex_specific_rate_acute_s, 
          Sex_specific_rate_chronic_s, Sex_specific_rate_diabetes_s, 
          sex_population_state)
-# Step 6: Calculate crude rates at the state level
+
+# Step 3: Calculate crude rates at the state level
 sex_rates_crude_statew <- state_data_sexw %>%
   group_by(State, Year) %>%
   summarise(
@@ -269,7 +261,7 @@ sex_rates_crude_statew <- state_data_sexw %>%
   ) %>%
   ungroup()
 
-# Step 7: Merge the final results into one data frame
+# Step 4: Merge the final results into one data frame
 final_rates_state_sexw <- sex_rates_crude_statew %>%
   left_join(sex_specific_rates_statew, by = c("State", "Year"))
 final_rates_state_sexw <- final_rates_state_sexw %>%
@@ -286,6 +278,7 @@ year_data_sexw<- cleaned_merged_data_sexw%>%
     Acute_Hosp = sum(`Acute Hospitalization`, na.rm = TRUE),
     Chronic_Hosp = sum(`Chronic Hospitalization`, na.rm = TRUE),
     Diabetes_Hosp = sum(`Diabetes Hospitalization`, na.rm = TRUE))%>% ungroup
+
 # Step 2: Calculate sex-specific rates for each year
 sex_specific_rates_yearw<-year_data_sexw %>%
   mutate( 
@@ -298,7 +291,7 @@ sex_specific_rates_yearw<-year_data_sexw %>%
          Sex_specific_rate_chronic_y, Sex_specific_rate_diabetes_y, 
          sex_population_year)
 
-# Step 6: Calculate crude rates at the year level
+# Step 3: Calculate crude rates at the year level
 sex_rates_crude_yearw <- year_data_sexw %>%
   group_by(Year) %>%
   summarise(
@@ -309,7 +302,7 @@ sex_rates_crude_yearw <- year_data_sexw %>%
   ) %>%
   ungroup()
 
-# Step 7: Merge the final results into one data frame
+# Step 4: Merge the final results into one data frame
 final_rates_year_sexw <- sex_rates_crude_yearw %>%
   left_join(sex_specific_rates_yearw, by = c("Year"))
 final_rates_year_sexw <- final_rates_year_sexw %>%
@@ -322,25 +315,12 @@ colnames(final_rates_county_sexw)
 colnames(final_rates_state_sexw)
 colnames(final_rates_year_sexw)
 
-duplicates_county_sexw <- final_rates_county_sexw %>%
-  group_by(State, Year, Sex) %>%
-  filter(n() > 1)
-
-
-duplicates_state_sexw <- final_rates_state_sexw %>%
-  group_by(State, Year, Sex) %>%
-  filter(n() > 1)
-
 final_rates_county_sex_distinctw <- final_rates_county_sexw %>%
   distinct(County, State, Year, Sex, .keep_all = TRUE)
 final_rates_state_sex_distinctw <- final_rates_state_sexw %>%
   distinct(State, Year, Sex, .keep_all = TRUE)
 final_data_sex_ratesw <- final_rates_county_sex_distinctw %>%
   left_join(final_rates_state_sex_distinctw, by = c("State", "Year", "Sex"))
-
-duplicates_year_sexw <- final_rates_year_sexw%>%
-  group_by( Year, Sex) %>%
-  filter(n() > 1)
 
 final_rates_year_sex_distinctw <- final_rates_year_sexw%>%
   distinct(Year, Sex, .keep_all = TRUE)
@@ -349,9 +329,9 @@ cleaned_data_sex_rates_finw<-final_data_sex_ratesw%>%
   left_join(final_rates_year_sex_distinctw, by=c("Year","Sex"))
 
 save(cleaned_data_sex_rates_finw, file="cleaned_data_sex_finalw.RData")
-load("cleaned_data_sex_finalw.RData")
 
-#GRAPHS- years (TABLE 1)
+
+#GRAPHS- years
 #ACSC
 ggplot(cleaned_data_sex_rates_fin %>% 
          filter(!is.na(Sex_specific_rate_acsc_s), !is.na(Sex)), 
@@ -383,7 +363,6 @@ ggplot(cleaned_data_sex_rates_finw %>%
   ) +
   theme_minimal()
 
-
 #ACUTE
 ggplot(cleaned_data_sex_rates_fin %>% 
          filter(!is.na(Sex_specific_rate_acute_s), !is.na(Sex)), 
@@ -414,7 +393,6 @@ ggplot(cleaned_data_sex_rates_finw %>%
     color = "Sex Category",
   ) +
   theme_minimal()
-
 
 #CHRONIC
 ggplot(cleaned_data_sex_rates_fin %>% 
@@ -478,7 +456,7 @@ ggplot(cleaned_data_sex_rates_finw %>%
   ) +
   theme_minimal()
 
-#Year total (TABLE 1)
+#Year total
 cleaned_data_sex_rates_fin%>% 
   filter(!is.na(Sex_specific_rate_acsc_y), !is.na(Sex))%>%
   filter(!is.na(Sex_specific_rate_acute_y), !is.na(Sex))%>%
@@ -537,7 +515,7 @@ cleaned_data_sex_rates_finw%>%
 cleaned_data_age_race_sex_merged<-cleaned_data_age_race_merged%>%
   left_join(cleaned_data_sex_rates_fin, by=c("GEOID","County","State","Year","Sex"))
 save(cleaned_data_age_race_sex_merged,file="cleaned_data_age_race_sex_merged.RData")
-load("cleaned_data_age_race_sex_merged.RData")
+
 #WORKING POPULATION DATASET
 cleaned_data_age_race_sex_mergedw<-cleaned_data_age_race_mergedw%>%
   left_join(cleaned_data_sex_rates_finw, by=c("GEOID","County","State","Year","Sex"))
